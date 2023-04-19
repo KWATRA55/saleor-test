d049cca552d155cec3b143b4d139ec6a10058293
KWATRA55 <shashwatkwatra55@gmail.com>
2023-04-19 18:28:19 +0530

    first commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,28 @@
-import '@/styles/globals.css'
+import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+import { relayStylePagination } from "@apollo/client/utilities";
+
+const client = new ApolloClient({
+  uri: "https://demo.saleor.io/graphql/",
+  cache: new InMemoryCache({
+    typePolicies: {
+      Query: {
+        fields: {
+          products: relayStylePagination([]),
+        },
+      },
+    }
+  }),
+});
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  )
 }
+
+//https://shashwat-store-8138.saleor.cloud/
